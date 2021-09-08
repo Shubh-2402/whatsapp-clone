62ccd21865d3f43ddc02f4668a3274b35b0cdc7a
Shubh-2402 <56020434+Shubh-2402@users.noreply.github.com>
2021-09-08 13:16:23 +0530

    intial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>MERN Whatsapp Clone</h1>
+      {/* Sidebar */}
+      {/* Chat */}
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAgtLYh_odabH9sH6hVAfjloE6Jfzy9k8I",
+    authDomain: "whatsapp-mern-350b6.firebaseapp.com",
+    projectId: "whatsapp-mern-350b6",
+    storageBucket: "whatsapp-mern-350b6.appspot.com",
+    messagingSenderId: "655507292381",
+    appId: "1:655507292381:web:1b5997ed1ff9462daa1b34"
+};
